145d1c19d212a56b71bb39eaddf3e4b91201eda5
vaibhavmahindru <500067728@stu.upes.ac.in>
2021-06-04 03:24:48 +0530

    delete api implemented

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,3 +26,23 @@ $("#update_user").submit(function(event){
 
 
 })
+
+if(window.location.pathname == "/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3000/api/users/delete/${id}`,
+            "method" : "DELETE"
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted Successfully!");
+                location.reload();
+            })
+        }
+
+    })
+}
